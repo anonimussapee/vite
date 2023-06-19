f47576eedcf9256a39e0a9e66ee8498e59cccab0
jdx <librededrogas420@gmail.com>
2023-06-19 02:30:48 -0500

    i added cards and logic of the component my orders

--- src/Pages/MyAccount/index.jsx
+++ src/Pages/MyAccount/index.jsx
@@ -3,11 +3,13 @@ import { Layout } from '../../Components/Layout';
 import { ContextUser } from '../../Components/ContextApi/contextUser';
 import { UserIcon } from '@heroicons/react/24/solid';
 import { Link } from 'react-router-dom';
+import { ContextMyOrders } from '../../Components/ContextApi/contextMyOrders';
 
 
 const MyAccount = () => {
 
   const context = useContext(ContextUser);
+  const contextOrders = useContext(ContextMyOrders);
 
 
     if(context.user.data.length>0){
@@ -20,12 +22,12 @@ const MyAccount = () => {
             <img className='w-52 h-52 rounded-full' src={`${context.user.data[0].image}`} alt={`${context.user.data[0].name}`} />
           </figure>
           <p>{context.user.data[0].name}</p>
-             <button className='w-36 h-auto p-2 border-[2px] border-gray-700 bg-gray-950 text-white' onClick={(e)=>{
-              e.preventDefault();
-            window.localStorage.clear();
-            location.reload();
+          <Link to='/sign-in'>
+             <button className='w-36 h-auto p-2 border-[2px] border-gray-700 bg-gray-950 text-white' onClick={()=>{
+            context.user.save([]);
+            contextOrders.myOrdersData.save([]);
           }}>Cerrar Session</button>
-          
+          </Link>
          
          </div>
         </Layout>
@@ -40,13 +42,13 @@ const MyAccount = () => {
             <UserIcon className='w-52 h-52'  />
           </figure>
           <p>Invitado</p>
-          <button className='w-36 h-auto p-2 border-[2px] border-gray-700 bg-gray-950 text-white' onClick={(e)=>{
-              e.preventDefault();
-
-            window.localStorage.clear();
-            location.reload();
+          <Link to='/sign-in'>
+          <button className='w-36 h-auto p-2 border-[2px] border-gray-700 bg-gray-950 text-white' onClick={()=>{
+            contextOrders.myOrdersData.save([]);
+            
+            
           }}>Borrar Historial</button>
-          
+          </Link>
          </div>
         </Layout>
     )
